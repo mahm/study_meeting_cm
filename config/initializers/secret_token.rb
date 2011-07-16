ac15ce9deaea1af3350b89de1a34d456d5d5efe1
mahm <rootmoon@gmail.com>
2011-07-17 08:36:16 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StudyMeetingCm::Application.config.secret_token = '2e5334d90c9d9651f174b22f4fd32b201caa63dd2ed214b18ff31488273159e86549c472a2ccdc9cb62db2cb4141234a02ab701bc21830f474f3b6faacbda524'
